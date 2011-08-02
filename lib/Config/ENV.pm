6e4eb5f3bdf2b7b3037caa8c8f32884c985e2961
cho45 (さとう) <cho45@lowreal.net>
2011-08-02 05:59:55 +0000

    Import of SATOH/Config-ENV-0.06 from CPAN.
    gitpan-cpan-distribution: Config-ENV
    gitpan-cpan-version:      0.06
    gitpan-cpan-path:         SATOH/Config-ENV-0.06.tar.gz
    gitpan-cpan-author:       SATOH
    gitpan-cpan-maturity:     released

--- lib/Config/ENV.pm
+++ lib/Config/ENV.pm
@@ -3,7 +3,7 @@ package Config::ENV;
 use strict;
 use warnings;
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 sub import {
 	my $class   = shift;
@@ -54,6 +54,7 @@ sub common ($) { ## no critic
 sub config ($$) { ## no critic
 	my ($name, $hash) = @_;
 	_data->{envs}->{$name} = $hash;
+	undef _data->{_merged}->{$name};
 }
 
 sub parent ($) { ## no critic
